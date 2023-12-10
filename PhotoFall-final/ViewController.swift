4f204c77db225f0fd93b063dbd85493816e6bcd3
liurunlin0213@live.com <liurunlin0213@live.com>
2023-12-09 16:04:38 -0800

    init

--- PhotoFall-final/ViewController.swift
+++ PhotoFall-final/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController, UITextViewDelegate, UITableViewDataSourc
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Comment
     }
 
 
